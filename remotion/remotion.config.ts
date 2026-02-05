d5301391465d2f12ea4fd6f3e6b68f1129694218
Bastian <bastian@besimplit.com>
2026-02-05 10:47:14 -0300

    fix: adapt Remotion config to new Config.* API

--- remotion/remotion.config.ts
+++ remotion/remotion.config.ts
@@ -1,6 +1,6 @@
 import {Config} from '@remotion/cli/config';
 
-Config.Rendering.setImageFormat('jpeg');
-Config.Output.setOverwriteOutput(true);
-Config.Output.setCodec('h264');
-Config.Browser.setConcurrency(2);
+Config.setImageFormat('jpeg');
+Config.setOverwriteOutput(true);
+Config.setCodec('h264');
+Config.setConcurrency(2);
